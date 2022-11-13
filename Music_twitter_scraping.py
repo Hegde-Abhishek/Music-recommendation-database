44e351b17ba9493c65dfa2ff39cf1fee27f1e09e
Hegde-Abhishek <hegde.ab@northeastern.edu>
2022-11-12 23:15:00 -0500

    final commit

---
+++ Music_twitter_scraping.py
@@ -0,0 +1,180 @@
+import tweepy # for twitter api
+import time
+import mysql.connector
+from mysql.connector import Error
+import re
+import requests
+from requests_oauthlib import OAuth1
+import json
+import datetime
+from datetime import datetime, timedelta
+
+#database connection
+def create_connection(host_name, user_name, user_password, db):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            db=db
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print("The error '{e}' occurred")
+    return connection
+connection = create_connection("localhost", "root", "admin", "music")
+cursor = connection.cursor()
+
+#  Authenticate
+APP_KEY = "xZ4OSNBnmkE9zN67GUBhPCqSB"
+APP_SECRET = "tb57HaWSEzgGUU5pDC43JkRVxZ9f3YbEUOF7AEWFjuiUIQ7GSU"
+USER_OAUTH_TOKEN = "1587541442548838400-yzsSqXr7g5vmAhLq5N3GPQapKhd1TH"
+USER_OAUTH_TOKEN_SECRET = "g5XvNK7Lg8wmyg6UCXE2QJ3FgpGrdXjKzcn5lKbR9aUe9"
+BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAABFZiwEAAAAA3mCApctlxrQEXK9%2FiGk6qNpTnXI%3D8ctkG7OyB1Tm7Mj4LSf07I6q5I4qPh1fmt0en0dUF06Z5Kwha1"
+authApi = OAuth1(APP_KEY, APP_SECRET,
+              USER_OAUTH_TOKEN, USER_OAUTH_TOKEN_SECRET)
+
+auth = tweepy.OAuthHandler(APP_KEY, APP_SECRET)
+api = tweepy.API(auth)
+
+#  Retrieve Tweets
+# sInput = input("Enter song name: ")
+# public_tweets = api.search_tweets('black', count=10)
+cursor.execute('SELECT * FROM songs s where s.song_popularity > 84')
+songs = cursor.fetchall()
+
+# Map song name and song id
+song_connect=  {}
+for song in songs:
+    song_connect[song[1]]=song[0]
+
+keywords=[]
+for i in songs:
+    keywords.append(i[1])
+    print(i[1])
+print(keywords)
+
+for names in keywords:
+
+    public_tweets = api.search_tweets(names)
+    song_id = song_connect[names]
+    print("------------------------------", public_tweets)
+
+    for tweet in public_tweets:
+        # Using tweets only from 2022-11-01 to 2022-11-12
+        t1=datetime.strptime("2022-11-01","%Y-%m-%d")
+        t2=datetime.strptime("2022-11-12","%Y-%m-%d") 
+        t1date=t1.date()
+        t2date=t2.date()
+        checkdate= tweet.created_at.date()
+    
+        if(checkdate<t1date and checkdate>t2date):
+            break
+        else: 
+            tweet_id = tweet.id
+            created_at = tweet.created_at
+            tweet_text = tweet.text
+            username = tweet.user.screen_name
+            name = tweet.user.name
+            userId = tweet.user.id
+            follower_count = tweet.user.followers_count
+            following_count = tweet.user.friends_count
+            twitter_handle = tweet.user.screen_name
+            profile_image_url = tweet.user.profile_image_url_https
+            description = tweet.user.description
+            userCreated_at = tweet.user.created_at
+            
+            # print("------here------", "type: ", type(tweet_id), "tweet id: ", tweet_id, "created_at: ", created_at, "tweet_text: ", tweet_text, "username: ", username, "name: ", name, "userId: ", userId, "follower_count: ", follower_count, "following_count: ", following_count, "twitter_handle: ", twitter_handle, "profile_image_url: ", profile_image_url, "description: ", description)
+            # break;
+            cursor.execute('''select * from tweets WHERE tweet_id = %s''', (tweet_id, ))
+            findTweet = cursor.fetchone()
+            print("findTweet----------", findTweet)
+            if findTweet:
+                print("Tweet already exists")
+            else:
+                cursor.execute('''select * from twitter_user where twitter_handle = %s''', (twitter_handle, ))
+                findUser = cursor.fetchone()
+                print("findUser----------", findUser)
+                if findUser:
+                    print("User already exists")
+                else:
+                    cursor.execute('''insert into twitter_user (twitter_handle, name,  description, followers_count, following_count, created_at) values (%s, %s, %s, %s, %s, %s)''', (twitter_handle, name, description, follower_count, following_count, userCreated_at))
+                
+                
+                tweets = api.get_status(tweet_id)
+                favorite_count = tweets.favorite_count
+                retweet_count = tweets.retweet_count
+
+                # likes_count = json.dumps(tweet['retweeted_status']['favorite_count']._json)
+                # retweet_count = tweet.retweet_count
+                cursor.execute('''insert into tweets (tweet_id, twitter_handle, tweet_text, created_at, song_id, likes, retweet) values (%s, %s, %s, %s, %s, %s, %s);''', (tweet_id, twitter_handle, tweet_text, created_at, song_id, favorite_count, retweet_count))
+                # cursor.execute('''insert into tweet_url (tweet_id, tweet_url) values (%s, %s)''', (tweet_id, "https://twitter.com/" + twitter_handle + "/status/" + str(tweet_id)))
+                
+                if(len(tweet.entities['user_mentions']) > 0):
+                    for mention in tweet.entities['user_mentions']:
+                        target_user = mention['screen_name']
+                        cursor.execute('''insert into tweet_mentions (tweet_id, source_user, target_user) values (%s, %s, %s)''', (tweet_id, twitter_handle, target_user))
+                
+                if(len(tweet.entities['hashtags']) > 0):
+                    for tag in tweet.entities['hashtags']:
+                        tag = tag['text']
+                        cursor.execute('''insert into tweet_tags (tweet_id, tags) values (%s, %s)''', (tweet_id, tag))
+
+                connection.commit()
+                print(cursor.rowcount, "was inserted.")
+
+
+# Q1    What user posted this tweet?
+anyTweet = input("Enter tweet id: ")
+first = cursor.execute("SELECT u.name, t.tweet_text FROM twitter_user as u INNER JOIN tweets as t ON u.twitter_handle = t.twitter_handle WHERE t.tweet_id = %s", (anyTweet, ))
+first = cursor.fetchone()
+print("Q1: ", first)
+
+# Q2    When did the user post this tweet?
+second = cursor.execute("SELECT u.name, t.tweet_text, t.created_at FROM twitter_user as u INNER JOIN tweets as t ON u.twitter_handle = t.twitter_handle WHERE t.tweet_id = %s", (anyTweet, ))
+second = cursor.fetchone()
+print("Q2: ", second)
+
+# Q3    What tweets have this user posted in the past 24 hours?
+anyUser = input("Enter user handle: ")
+now = datetime.now()
+prev = now + timedelta(days=-1)
+now = datetime.strftime(now, "%Y-%m-%d %H:%M:%S")
+prev = datetime.strftime(prev, "%Y-%m-%d %H:%M:%S")
+
+print("time------", type(prev), now)
+fetchThree = api.user_timeline(screen_name=anyUser, count=100)
+for tweet in fetchThree:
+    created_at_date = datetime.strftime(tweet.created_at, "%Y-%m-%d %H:%M:%S")
+    print(created_at_date)
+    if(created_at_date > prev and created_at_date < now):
+        print("tweet in 24 hours")
+        cursor.execute("insert into tweets (tweet_id, twitter_handle, tweet_text, created_at, likes, retweet) values (%s, %s, %s, %s, %s, %s);", (tweet.id, anyUser, tweet.text, tweet.created_at, tweet.favorite_count, tweet.retweet_count))
+        connection.commit()
+        print(cursor.rowcount, "was inserted.")
+    else:
+        print("tweet not in 24 hours")
+qthree = cursor.execute("SELECT u.name, t.tweet_text, t.created_at FROM twitter_user as u INNER JOIN tweets as t ON u.twitter_handle = t.twitter_handle WHERE u.twitter_handle = %s and t.created_at BETWEEN %s AND %s", (anyUser, prev, now))
+qthree = cursor.fetchall()
+print("Q3: ", qthree)
+
+# Q4    How many tweets have this user posted in the past 24 hours?
+fourth = cursor.execute("SELECT u.name, count(t.tweet_text), t.created_at FROM twitter_user as u INNER JOIN tweets as t ON u.twitter_handle = t.twitter_handle WHERE u.twitter_handle = %s and t.created_at BETWEEN %s AND %s", (anyUser, prev, now))
+fourth = cursor.fetchall()
+print("Q4: ", fourth)
+
+# Q5    When did this user join Twitter?
+qfive = cursor.execute("SELECT u.twitter_handle, u.created_at FROM twitter_user as u where u.twitter_handle = %s", (anyUser, ))
+qfive = cursor.fetchall()
+print("Q5: ", qfive)
+
+# Q6    What keywords/ hashtags are popular?
+qsix = cursor.execute("SELECT t.tags, count(t.tags) FROM tweet_tags as t GROUP BY t.tags ORDER BY count(t.tags) DESC LIMIT 10")
+qsix = cursor.fetchall()
+print("Q6: ", qsix)
+
+# Q7    What tweets are popular?
+qseven = cursor.execute("SELECT t.tweet_text, t.likes, t.retweet FROM tweets as t ORDER BY t.retweet DESC LIMIT 10")
+qseven = cursor.fetchall()
+print("Q7: ", qseven)
--- README.md
+++ README.md
@@ -17,3 +17,19 @@ Use-case for authorized users(admin):
 1. Can add and remove albums from the playlists and database.
 2. Add new music or albums to the database.
 3. The current authorized user or administrator can see the updates to the uploaded data.
+
+Steps to run the system :
+Run MySQL Workbench
+Upload “Music_Recommendaion_System.sql” in it
+Connect to local server.
+Once workbench initiates, go to file and click “Run SQL Script…”
+Select the “Music_Recommendaion_System.sql” file extracted from the Zip Folder
+Database with tables & columns is ready
+Check the schemas to view the columns
+If the columns are not visible there will be a refresh button just below the schema button.
+Refer document “Relational Algebra for twitter queries” for 7 twitter queries
+Refer document "Use cases with sql queries" for 10 use cases.
+The Entity- Relationship Diagram is available in the png file - “ER Diagram.png”
+
+
+(Note: Dataset is included in the folder 'Data' for reference)
